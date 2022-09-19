629e056bfbd2a64cafb20e406e0e3e19f13181df
Kazem Mirzaei <k90mirzaei@gmail.com>
2022-09-20 00:37:38 +0800

    Set up React router

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Outlet, Route, Routes } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Routes>
+        <Route path="/" element={<div>Hello guyes!</div>} />
+        <Route
+          path="talks"
+          element={
+            <div>
+              <h1>Hi</h1>
+              <Outlet />
+            </div>
+          }
+        />
+      </Routes>
+    </>
   );
 }
 
